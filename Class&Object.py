d7de0eea8f1599fe6ab904aa112de5594e0d0c9a
Innocentprogrammer <mratyunjaysaxena8859@gmail.com>
2024-07-05 20:46:38 +0530

    Class & Object

---
+++ Class&Object.py
@@ -0,0 +1,5 @@
+# Q1 Define a python class Person with instance object variables name and age. Set Instance object variables in __init__() nethod. Also define show() method to display name and age of a person.
+# Q2 Define a class Circle with instance object variable raduis. Provide setter and getter for radius. Also define getArea() and getCircumference() methods.
+# Q3 Define a class Rectangle with length and breadth as instance object variables. Provide setDimensions(), showDimensions() and getArea() method in it.
+# Q4 Define a class Book with instance object variables bookid, title, and price. Initialise them via __init__() method. Also define method to show book variables.
+# Q5 Define a class Team with instance object variable a list of team member names. Provide methods to input member names and display member names.
