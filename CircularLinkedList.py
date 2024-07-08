e665a11b7e03716944c72e744a7142f62ce5f5ed
Innocentprogrammer <mratyunjaysaxena8859@gmail.com>
2024-07-08 17:21:41 +0530

    Update of Day3

---
+++ CircularLinkedList.py
@@ -0,0 +1,12 @@
+# Q1. Define a class Node to describe a node of Circular linked list.
+# Q2. Define a class CLL to implement Circular Linked List with __init__() method to create and initialise start reference variable
+# Q3. Define a method is_empty() to check if linked list is empty in CLL class.
+# Q4. In class CLL, define a method insert_at_start() to insert an element at the starting of the list.
+# Q5. In class CLL, define a method insert_at_last() to insert an element at the end of the list.
+# Q6. In class CLL, define a method search() to search an node with specified element value.
+# Q7. In class CLL, define a method insert_after() to insert new node after a given node of the list.
+# Q8. In class CLL, define a method to print all the element of list.
+# Q9. In class CLL, define a method delete_first() to delete first element from the list.
+# Q10. In class CLL, define a method delete_last() to delete last element from the list.
+# Q11. In class CLL, define a method delete_item() to delete specified element from the list.
+# Q12. In class CLL, implement iterator for CLL to access all the elements of the list in a sequence.
